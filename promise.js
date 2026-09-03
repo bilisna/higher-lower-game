eefd355c38dadabe4776a2ac3d7e0569b5ff2df9
bilisna <bilisnatamrakar@gmail.com>
2026-09-03 08:36:19 +0545

    promise study

---
+++ promise.js
@@ -0,0 +1,13 @@
+const checkLogin=new Promise(
+    (resolve,reject)=>{
+        let loggedIn=true;
+        if(loggedIn){
+            resolve("welcome you are logged in")
+        }
+        else{
+            reject("sorry you are not logged in")
+        }
+    }   
+)
+checkLogin.then((result)=>{console.log(result)})
+checkLogin.catch((error)=>{console.log(error)})
